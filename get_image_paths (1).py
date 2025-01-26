9bbd91ecafc3e3801dbab556e886d25e2436f6f6
nehashahu11 <nvshahu210@gmail.com>
2025-01-26 20:41:33 +0530

    files for vocab size 300, 400 and 450 uploaded

---
+++ get_image_paths (1).py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+
+def get_image_paths(data_path, categories, num_train_per_cat,num_val_per_cat,num_test_per_cat):
+    num_categories = len(categories)
+
+    train_image_paths = []
+    test_image_paths = []
+    val_image_paths = []
+
+    train_labels = []
+    test_labels = []
+    val_labels = []
+
+    for category in categories:
+
+        image_paths = glob(os.path.join(data_path, 'train', category, '*.tif'))
+        for i in range(num_train_per_cat):
+            train_image_paths.append(image_paths[i])
+            train_labels.append(category)
+
+        image_paths = glob(os.path.join(data_path, 'val', category, '*.tif'))
+        for i in range(num_val_per_cat):
+            val_image_paths.append(image_paths[i])
+            val_labels.append(category)
+
+        image_paths = glob(os.path.join(data_path, 'test', category, '*.tif'))
+        for i in range(num_test_per_cat):
+            test_image_paths.append(image_paths[i])
+            test_labels.append(category)
+
+    return train_image_paths,val_image_paths, train_labels,val_labels
